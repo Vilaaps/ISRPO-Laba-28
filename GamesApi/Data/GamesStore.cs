e426560a1a604e75acd8aeef14d8fa4c82778316
vika <vikatekutova7@gmail.com>
2026-04-14 13:10:03 +0300

    Step 5: GamesController with GET, POST, DELETE, PUT

---
+++ GamesApi/Data/GamesStore.cs
@@ -0,0 +1,28 @@
+using GamesApi.Models;
+using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+namespace GamesApi.Data;
+
+public static class GamesStore {
+        private static int _nextId = 4;
+    public static List<Game> Games { get; } = new() {
+            new Game {
+                Id = 1,
+                Title = "The Elder Scrolls IV: Oblivion",
+                Genre = "RPG",
+                ReleaseYear = 2006,
+            },
+            new Game {
+                Id = 2,
+                Title = "The Witcher 3 : Wild Hunt",
+                Genre = "RPG",
+                ReleaseYear = 2015,
+            },
+            new Game {
+                Id = 3,
+                Title = "GTA : San Andreas",
+                Genre = "RPG",
+                ReleaseYear = 2004,
+            },
+        };
+    public static int NextId() => _nextId++;
+    }
